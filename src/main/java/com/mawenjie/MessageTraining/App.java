7f13f933ab5e079e4b2c401629726d27afca2b28
mawenjie <754825416@qq.com>
2018-03-09 11:49:28 +0800

    init

---
+++ src/main/java/com/mawenjie/MessageTraining/App.java
@@ -0,0 +1,13 @@
+package com.mawenjie.MessageTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
